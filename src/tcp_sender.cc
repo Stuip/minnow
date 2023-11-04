726de6ef588ccdfde1f2b1100b54943777373a4f
Stuip <tangjmee@gmail.com>
2023-11-04 10:58:47 +0800

    Pass the TCP Sender!!! Damn Hard!!!

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -1,5 +1,6 @@
 #include "tcp_sender.hh"
 #include "tcp_config.hh"
+#include <iostream>
 
 #include <random>
 
@@ -7,47 +8,145 @@ using namespace std;
 
 /* TCPSender constructor (uses a random ISN if none given) */
 TCPSender::TCPSender( uint64_t initial_RTO_ms, optional<Wrap32> fixed_isn )
-  : isn_( fixed_isn.value_or( Wrap32 { random_device()() } ) ), initial_RTO_ms_( initial_RTO_ms )
+  : isn_( fixed_isn.value_or( Wrap32 { random_device()() } ) ),
+  initial_RTO_ms_( initial_RTO_ms )
 {}
 
 uint64_t TCPSender::sequence_numbers_in_flight() const
 {
   // Your code here.
-  return {};
+  return next_seqno_-recev_seqno_;
 }
 
 uint64_t TCPSender::consecutive_retransmissions() const
 {
   // Your code here.
-  return {};
+  return ncr_;
 }
 
 optional<TCPSenderMessage> TCPSender::maybe_send()
 {
-  // Your code here.
-  return {};
+    if (messages_.empty()) {
+        return {};
+    }
+    TCPSenderMessage message;
+    message = messages_.front();
+    messages_.pop_front();
+    // 放入到已经发送但是没有ACK的数据报集合中
+    outstanding_messages_.push_back(message);
+    if (!active_) {
+        // 启动定时器
+        active_ = true;
+    }
+    return message;
 }
 
 void TCPSender::push( Reader& outbound_stream )
 {
-  // Your code here.
-  (void)outbound_stream;
+    // 如果fin已经发送的话，说明byteStream中并没有多余的字节
+    if (fin_) {
+        return;
+    }
+    const uint64_t cur_window_size = window_size_==0?1:window_size_;
+    while (cur_window_size > sequence_numbers_in_flight()) {
+        // 首先要保证窗口有还有空余位置去发送
+        TCPSenderMessage message;
+        if (!syn_) {
+            syn_ = true;
+            message.SYN = true;
+        }
+
+        message.seqno = isn_ + next_seqno_;
+
+        auto len = min(min(TCPConfig::MAX_PAYLOAD_SIZE,
+                           static_cast<size_t >(outbound_stream.bytes_buffered())), cur_window_size-sequence_numbers_in_flight());
+
+
+        read(outbound_stream, len, message.payload);
+
+        if (!fin_ && outbound_stream.is_finished() &&
+            len + message.SYN + sequence_numbers_in_flight() < cur_window_size) {
+            fin_ = true;
+            message.FIN = true;
+        }
+
+        if (message.sequence_length() == 0) {
+            break;
+        }
+
+        messages_.push_back(message);
+        next_seqno_ += message.sequence_length();
+        if (message.FIN || outbound_stream.bytes_buffered() == 0) {
+            break;
+        }
+    }
 }
 
+/***
+ * 产生一个长度为0的数据报，其中序列号设置正确。
+ * 主要的目的是为了当receiver发送一个TCPReceiverMessage，Sender产生一个正确seqno的TCPSender
+ */
 TCPSenderMessage TCPSender::send_empty_message() const
 {
   // Your code here.
-  return {};
+    TCPSenderMessage message{};
+    message.seqno = isn_ + next_seqno_;
+    return message;
 }
 
+/**
+ * 这个函数主要是浏览sender已经发送的但是没有acknowledge的segment的集合，
+ * 将那些已经被sender acknowledge的从集合中删除
+ */
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
-  // Your code here.
-  (void)msg;
+    if (msg.ackno.has_value()) {
+        auto rv_seqno = msg.ackno->unwrap(isn_, next_seqno_);
+        if (rv_seqno > next_seqno_ || rv_seqno < recev_seqno_) {
+            // 如果ACK的receive_seqno大于next_seqno的话，则直接返回
+            // 因为receiver接受到还没有发送的数据。
+            return;
+        }
+        recev_seqno_ = msg.ackno->unwrap(isn_, next_seqno_);
+    }
+
+    window_size_ = msg.window_size;
+
+    while (!outstanding_messages_.empty()) {
+        auto segment = outstanding_messages_.front();
+        auto last_seqno = (segment.seqno + segment.sequence_length()).unwrap(isn_, next_seqno_);
+        if (last_seqno <= recev_seqno_) {
+            outstanding_messages_.pop_front();
+            // 如果sender收到的有效的ackno，则需要你重置定时器
+            if (window_size_ != 0){
+                timestamp_ = 0;
+                cur_RTO_ = initial_RTO_ms_;
+                ncr_ = 0;
+            }
+        } else {
+            break;
+        }
+    }
+
+    // 如果已发送但未认可的segment没有了，就关闭定时器
+    active_ = !outstanding_messages_.empty();
+
 }
 
 void TCPSender::tick( const size_t ms_since_last_tick )
 {
   // Your code here.
-  (void)ms_since_last_tick;
+    if (active_) {
+        timestamp_ += ms_since_last_tick;
+        if (timestamp_ >= cur_RTO_ && !outstanding_messages_.empty()) {
+            // 定时器已经失效，所以需要重新传递最久的数据报
+            messages_.push_back(outstanding_messages_.front());
+            if (window_size_ > 0) {
+                ncr_ += 1;
+                cur_RTO_ = pow(2, ncr_) * initial_RTO_ms_;
+            }
+            timestamp_ = 0;
+        }
+    }
 }
+
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -4,10 +4,38 @@
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
 
+#include <exception>
+#include <functional>
+
+
+/**
+ * TCPSender responsiblity to:
+ *      1. 记录receiver的window大小和acknos
+ *      2. 尽可能地填补receiver的window,通过从ByteStream读取,创建新的TCP segment,之后发送它们.
+ *          发送者会一直发送segments,直到window满了或者是outbound已经没有数据传输了
+ *      3. 记录那些segment已经发送了,但是没被receiver
+ *      4. 如果已经发送的segment,在发送后经过过足够的时间而且还没有收到确认,那就重新发送这些未确认的segment
+ */
+
+
 class TCPSender
 {
+private:
   Wrap32 isn_;
   uint64_t initial_RTO_ms_;
+  bool syn_ = false;
+  bool fin_ = false;
+  uint64_t next_seqno_{0};   // 记录sender已经发送多少个字节（包含第一次握手）
+  uint64_t recev_seqno_{0};  // 记录sender已经接受了多少个字节）
+  uint16_t window_size_{1};  // receiver的滑动窗口的大小
+  std::deque<TCPSenderMessage> outstanding_messages_{}; // sender已经发送，但是还没有收到receiver回复的ack
+  std::deque<TCPSenderMessage> messages_{}; // Sender 准备发送的sender messages
+  uint64_t ncr_{0}; // the number of consecutive retransmissions
+
+  // 定时器
+  size_t timestamp_{0}; // 只从上次调用已经过了多久了
+  bool active_{false};
+  uint64_t cur_RTO_{initial_RTO_ms_};
 
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
@@ -32,3 +60,4 @@ public:
   uint64_t sequence_numbers_in_flight() const;  // How many sequence numbers are outstanding?
   uint64_t consecutive_retransmissions() const; // How many consecutive *re*transmissions have happened?
 };
+
